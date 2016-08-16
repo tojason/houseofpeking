1c9b5fd2381b9996a8853c3b72e6f759ffff54c6
Jason <junsitu2@illinois.edu>
2016-08-16 17:10:02 -0500

    update css

--- js/script.js
+++ js/script.js
@@ -6,68 +6,7 @@ $(function() {
         // wrap: false,
         // keyboard: false
     });
-    
-
-    
 });
-
-$(document).ready(function() {
-
-    $('#middle').css('display', 'none');
-
-    $('#middle').fadeIn(1500);
-
-
-
-    $('.link').click(function(event) {
-
-        event.preventDefault();
-
-        newLocation = this.href;
-
-        $('body').fadeOut(1000, newpage);
-
-    });
-
-
-
-    function newpage() {
-
-        window.location = newLocation;
-
-    }
-    
-    $('a[href^="#"]:not([href="#"])').click(function(e) {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            // this.hash reads href attr of this
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 'slow');
-                return false;
-            }
-        }
-        e.preventDefault();
-    });
-    
-//    var navOffset = $('#nav-wrap').offset().top
-//    $(window).scroll(function() {
-//        var scrollPos = jQuery(window).scrollTop();
-//        
-//        if (scrollPos >= navOffset) {
-//            $("#nav-wrap").addClass("fixed col-lg-offset-3");
-//            $("#navigationmenu").addClass("nav-scroll");
-//            
-//        } else {
-//            $("#nav-wrap").removeClass("fixed fixed col-lg-offset-3");
-//            $("#navigationmenu").removeClass("nav-scroll");
-//        }
-//    });
-
-});
-
 
 $(window).ready(function(){
 //    var topBtn = document.getElementById('return-top');
